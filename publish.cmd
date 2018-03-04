a97bb315da4772f0217900741fea9421fd22cc64
Maksym Voloshyn <mx.voloshin@gmail.com>
2018-03-04 17:02:05 +0200

    Add project files.

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.SimpleEchoBot.sln -p:DeployOnBuild=true -p:PublishProfile=redirector-9070-Web-Deploy.pubxml -p:Password=en4bseNiqgqxgME2kB2mDjx7F5lfhlc0x21EMgnTfvDbhEF16ZgqFjfs0psr
+
